c31ec1963ca1787d068b1559471344a909543efb
Roberto Poblete <rpobleted@live.cl>
2023-04-03 19:08:05 -0400

    03-04-2023 - AM

--- Ejercicios_Practicos/Ejercicio_01/Ejercicio_01.java
+++ Ejercicios_Practicos/Ejercicio_01/Ejercicio_01.java
@@ -2,15 +2,15 @@ package Ejercicios_Practicos.Ejercicio_01;
 
 import Ejercicios_Practicos.Ejercicio_01.Services.PerroService;
 
-
 public class Ejercicio_01 {
 
     public static void main(String[] args) {
 
         PerroService sv = new PerroService();
 
-       sv.ingresoRaza();
-       sv.mostrarRazas();
+        sv.ingresoPerro();
+        sv.eliminarPerro();
+        sv.mostrarPerros();
 
     }
 }
--- Ejercicios_Practicos/Ejercicio_01/Services/PerroService.java
+++ Ejercicios_Practicos/Ejercicio_01/Services/PerroService.java
@@ -1,7 +1,11 @@
 package Ejercicios_Practicos.Ejercicio_01.Services;
 
+
 import Ejercicios_Practicos.Ejercicio_01.Entidades.Perro;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
 import java.util.Scanner;
 
 public class PerroService {
@@ -9,25 +13,85 @@ public class PerroService {
     Scanner leer = new Scanner(System.in).useDelimiter("\n");
     ArrayList<Perro> listaRazasPerro = new ArrayList();
 
-    public Perro ingresoRaza() {
+    /**
+     * Método para creación de objetos tipo Perro que seran guardados en una
+     * Lista, incluye 2 atributos nombre y raza.
+     *
+     * @return Objeto
+     */
+    public Perro ingresoPerro() {
+        System.out.println("****   CREACION DE PERROS    ****");
         String opc;
         Perro objeto;
         do {
             objeto = new Perro();
-            System.out.println("Ingrese el nombre del perro");
+            System.out.print("Ingrese el nombre del perro : ");
             objeto.setNombre(leer.next());
-            System.out.println("Ingrese la raza del perro");
+            System.out.print("Ingrese la raza del perro  : ");
             objeto.setRaza(leer.next());
             listaRazasPerro.add(objeto);
             System.out.println("Presione S para continuar, sino presione cualquier otra tecla");
             opc = leer.next();
         } while (opc.equalsIgnoreCase("s"));
+        mostrarPerros();
         return objeto;
     }
 
-    public void mostrarRazas() {
-        for (Perro var : listaRazasPerro) {
-            System.out.println(var);
+    /**
+     * Método para recorrer la lista mediante forEach.
+     */
+    public void mostrarPerros() {
+        System.out.println("****   LISTA COMPLETA    ****");
+        ordenarPorRaza(listaRazasPerro);
+        listaRazasPerro.forEach((var) -> System.out.println(var));
+    }
+
+    /**
+     * Método para recorrer la lista mediante ITERATOR, ingresar un valor String
+     * para raza y cuando lo encuentre borrar la posición de la lista. 1) Cuando
+     * no encuentra el valor, entrega mensaje error y muestra lista. 2) Cuando
+     * encuentra los valores, entrega la cantidad de registros que eliminar y
+     * luego muestra lista actualizada
+     */
+    public void eliminarPerro() {
+        System.out.println("****   MENU DE ELIMINACION DE PERROS    ****");
+        System.out.println("Ingresa la raza del perro que quieres eliminar : ");
+        String aux = leer.next();
+        System.out.println("Ingresa el nombre del perro que quieres eliminar : ");
+        String aux1 = leer.next();
+        int coincidencias = 0;
+
+        Iterator<Perro> it = listaRazasPerro.iterator();
+        while (it.hasNext()) {
+            Perro perroActual = it.next(); // Almacenar el objeto que trae de la lista (el valor actual) y lo guardo en un objeto aux.
+            if (perroActual.getRaza().equals(aux) && perroActual.getNombre().equals(aux1)) {
+                it.remove();
+                coincidencias++;
+            }
         }
+
+        if (coincidencias == 0) {
+            System.out.println("No se encuentra el valor buscado");
+        }
+
+        if (coincidencias > 0) {
+            System.out.println(coincidencias + "Registros eliminados");
+        }
+        mostrarPerros();
+    }
+
+
+        public static void ordenarPorRaza(ArrayList<Perro> listaRazasPerros) {
+        
+        // Definimos un comparador para ordenar por raza
+        Comparator<Perro> comparadorRaza = new Comparator<Perro>() {
+            @Override
+            public int compare(Perro perro1, Perro perro2) {
+                return perro1.getRaza().compareTo(perro2.getRaza());
+            }
+        };
+        
+        // Ordenamos la lista utilizando el comparador
+        Collections.sort(listaRazasPerros, comparadorRaza);
     }
 }
